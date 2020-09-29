4d57106cd8aede75ff2dc713508aa7de68cfca50
Roman Iftodi <romeo8881@gmail.com>
2020-09-29 15:23:43 +0300

    format

--- contracts/BZxFlashLoaner.sol
+++ contracts/BZxFlashLoaner.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IToken.sol"; 
+import "./IToken.sol";
 
 contract BZxFlashLoaner is Ownable {
     function initiateFlashLoanBzx(
@@ -40,24 +40,21 @@ contract BZxFlashLoaner is Ownable {
         address iToken,
         uint256 loanAmount
     ) external {
-        emit BalanceOf(
-            IERC20(loanToken).balanceOf(address(this))
-        );
+        emit BalanceOf(IERC20(loanToken).balanceOf(address(this)));
         emit ExecuteOperation(loanToken, iToken, loanAmount);
         repayFlashLoan(loanToken, iToken, loanAmount);
     }
 
-    function doStuffWithFlashLoan(address token, address iToken, uint256 amount) external onlyOwner {
-
-        emit BalanceOf(
-            IERC20(token).balanceOf(address(this))
-        );
+    function doStuffWithFlashLoan(
+        address token,
+        address iToken,
+        uint256 amount
+    ) external onlyOwner {
+        emit BalanceOf(IERC20(token).balanceOf(address(this)));
 
         initiateFlashLoanBzx(token, iToken, amount);
 
-        emit BalanceOf(
-            IERC20(token).balanceOf(address(this))
-        );
+        emit BalanceOf(IERC20(token).balanceOf(address(this)));
 
         // after loan checks and what not.
     }
